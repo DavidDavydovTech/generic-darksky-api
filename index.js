24976e8b59eb6a300c2a3bd23a8631fdbdeb6cf4
David J. Davydov <davydovjd611@gmail.com>
2021-02-15 12:29:37 -0500

    Create index.js and some boilerplate code

---
+++ index.js
@@ -0,0 +1,22 @@
+// -- Package imports --
+const express = require('express');
+
+
+// -- Setup --
+const app = express();
+const router = express.Router();
+
+
+// -- Main --
+
+
+// -- Launch App --
+if (true) {
+  app.listen(9001);
+}
+
+
+// -- Exports --
+module.exports = {
+  WeatherApp: app,
+};
