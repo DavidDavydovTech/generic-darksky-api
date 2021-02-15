7bc4084973971cf73d381c51584d70551015d7db
David J. Davydov <davydovjd611@gmail.com>
2021-02-15 12:40:05 -0500

    Implement dotenv and the environment variables

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 // -- Package imports --
+const env = require('dotenv').config();
 const express = require('express');
 
 
 // -- Setup --
+const {
+  GENERIC_DARKSKY_API_SHOULD_RUN,
+  GENERIC_DARKSKY_API_PORT,
+} = env;
 const app = express();
 const router = express.Router();
 
@@ -11,8 +16,15 @@ const router = express.Router();
 
 
 // -- Launch App --
-if (true) {
-  app.listen(9001);
+if (GENERIC_DARKSKY_API_SHOULD_RUN === true) {
+  try {
+    app.listen(GENERIC_DARKSKY_API_PORT);
+  } catch (err) {
+    console.error(
+      'Got an error while trying to launch the app (is your GENERIC_DARKSKY_API_PORT a number?).',
+      err
+    );
+  }
 }
 
 
