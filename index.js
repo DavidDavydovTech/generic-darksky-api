b8610e5972564c002f49055b3ddc4f1300c9cc00
David J. Davydov <davydovjd611@gmail.com>
2021-02-15 13:30:34 -0500

    Make the API pull from the .env, then process.env
    The index.js will now pull defaults found in .env, and then overwrite
    the defaults with variables supplied by process.env.

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 // -- Package imports --
-const env = require('dotenv').config();
+const dotenv = require('dotenv');
 const express = require('express');
 
 
 // -- Setup --
+// Get default ENV from
+const env = Object.assign( dotenv.config().parsed || {}, process.env);
 const {
-  GENERIC_DARKSKY_API_SHOULD_RUN,
   GENERIC_DARKSKY_API_PORT,
+  GENERIC_DARKSKY_API_SHOULD_RUN
 } = env;
 const app = express();
 const router = express.Router();
@@ -15,13 +17,16 @@ const router = express.Router();
 // -- Main --
 
 
+
 // -- Launch App --
-if (GENERIC_DARKSKY_API_SHOULD_RUN === true) {
+if (GENERIC_DARKSKY_API_SHOULD_RUN === '1') {
   try {
-    app.listen(GENERIC_DARKSKY_API_PORT);
+    const port = Number.parseInt(GENERIC_DARKSKY_API_PORT);
+    const backupPort = 9001;
+    app.listen( port || backupPort );
   } catch (err) {
     console.error(
-      'Got an error while trying to launch the app (is your GENERIC_DARKSKY_API_PORT a number?).',
+      'Got an error while trying to launch the app (is your GENERIC_DARKSKY_API_PORT a number?).\n\n',
       err
     );
   }
