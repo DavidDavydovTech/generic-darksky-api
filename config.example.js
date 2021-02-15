0eff3022ce53d937fc59d9c996d1820a30bb85be
David J. Davydov <davydovjd611@gmail.com>
2021-02-15 18:49:42 -0500

    Update example config

--- config.example.js
+++ config.example.js
@@ -1,6 +1,7 @@
 module.exports = {
   'APIKeys': {
     'positionStack': 'YOUR API KEY HERE',
-    'darkSky': 'YOUR API KEY HERE'
+    'darkSky': 'YOUR API KEY HERE',
+    'pexels': 'YOUR API KEY HERE',
   }
 };
