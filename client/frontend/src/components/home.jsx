164a4f900e90da459cf696571c2203b4dbcfc883
David J. Davydov <davydovjd611@gmail.com>
2021-02-15 22:25:45 -0500

    Rework homepage

--- client/frontend/src/components/home.jsx
+++ client/frontend/src/components/home.jsx
@@ -1,9 +1,34 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom'
+import axios from 'axios';
 
 const Home = (props) => {
+  const [ address, setAddress ] = useState('');
+  const history = useHistory();
+  // In a normal project you'd use redux or something to set the 
+  // background image, but this project is too small for that to be 
+  // worth setting up.
+  document.body.style.backgroundImage = `url(https://source.unsplash.com/1920x1280/?bright,sunny,scenery)`
 
   return (
-    <h1>Hi</h1>
+    <div className="center-col">
+      <h1 className="title">Weather App</h1>
+      <form onSubmit={ e => e.preventDefault() }>
+        <input
+          className="px-6 py-4 text-3xl m-4 rounded-xl font-sans shadow-xl"
+          placeholder="Type in an address"
+          name="address"
+          value={address}
+          onChange={ e => setAddress(e.target.value)}
+        />
+      </form>
+      <button 
+        className="rounded-full bg-white text-3xl mt-6 p-4 shadow-xl transform duration-700 hover:scale-150 hover:bg-green-200 hover:animate-ping"
+        onClick={() => history.push(`/forecast?address=${address}`) }
+      >
+        🔍
+      </button>
+    </div>
   );
 };
 
