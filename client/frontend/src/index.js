5e05af7d45fb1e8e34401fa00bec82e18ee0ea33
David J. Davydov <davydovjd611@gmail.com>
2021-02-15 21:01:07 -0500

    Add app id to the main app div

--- client/frontend/src/index.js
+++ client/frontend/src/index.js
@@ -5,5 +5,6 @@ import "./styles.css";
 
 
 const mountNode = document.createElement('div');
+mountNode.id = 'app';
 document.body.appendChild(mountNode);
 ReactDOM.render(<App name="Jane" />, mountNode);
